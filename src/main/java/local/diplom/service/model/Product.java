67004cefceb1ee09360220a6d8e5436bc01fba45
david <chaava01@gmail.com>
2016-02-22 13:28:42 +0300

    First persist

--- src/main/java/local/diplom/service/model/Product.java
+++ src/main/java/local/diplom/service/model/Product.java
@@ -1,9 +1,35 @@
 package local.diplom.service.model;
 
-import local.diplom.service.abstracts.Entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created  by david on 21.02.16
  */
-public class Product extends Entity {
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String name;
+
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
