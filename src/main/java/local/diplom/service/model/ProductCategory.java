cab923d9c68d278a4066159db1a13760de57dfe9
david <chaava01@gmail.com>
2016-02-27 01:38:16 +0300

    .gitignore is now working

---
+++ src/main/java/local/diplom/service/model/ProductCategory.java
@@ -0,0 +1,33 @@
+package local.diplom.service.model;
+
+import local.diplom.service.abstracts.EntityInterface;
+
+import javax.persistence.*;
+
+/**
+ * Created by david on 27.02.16.
+ */
+@Entity
+@NamedQuery(name = "ProductCategory.getAll", query = "SELECT c from ProductCategory c")
+public class ProductCategory implements EntityInterface {
+    private Long id;
+    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
