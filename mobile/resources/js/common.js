7f9d53b3cdf7cbe29824df4b7d3fab56cacab7b3
david <chaava01@gmail.com>
2016-02-28 16:54:34 +0300

    altet field

--- mobile/resources/js/common.js
+++ mobile/resources/js/common.js
@@ -2,18 +2,17 @@ function main(){
 	$( "#menu" ).load("menu.html");
 	$( "#footer" ).load("footer.html");
 	$( "#menu" ).load("menu.html");
-	route();
+	this.route();
 	$(window).bind('hashchange', function() {
-		route();
+		this.route();
 	});
-
 }
 function route(){
 	var url = document.URL;
 	if(url.indexOf('#!')>0){
 		if (url.indexOf('?')>0) var path = url.substring(url.indexOf('#!')+1, url.indexOf('?')) 
 			else var path = url.substring(url.indexOf('!')+1);
-		loadToContent(path);
+		this.loadToContent(path);
 	} else loadToContent('home')
 }
 function loadToContent(page){
@@ -23,37 +22,11 @@ function loadToContent(page){
 function drawContent(phones, tpl, to){
 	$('#'+tpl).tmpl(phones).appendTo('#'+to);
 }
-function sendEmail(html){
-	$.ajax({
-	  type: "POST",
-	  url: "https://mandrillapp.com/api/1.0/messages/send.json",
-	  data: {
-	    key: 'plv6707RFJSqLan-KWMYFg',
-	    message: {
-	      from_email: 'chdavi@ya.ru',
-	      to: [
-	          {
-	            email: 'chdavi@ya.ru',
-	            name: 'RECIPIENT NAME (OPTIONAL)',
-	            type: 'to'
-	          }
-	        ],
-	      autotext: 'true',
-	      subject: 'Заявка',
-	      html:  html
-	    }
-	  }
-	 }).done(function(response) {
-	   console.log(response); // if you're into that sorta thing
-	   if(response && response[0].status == "sent"){
-	   		location.href = "#!send_email_succ";
-	   } else {
-	   		location.href = "#!send_email_err";
-	   }
-	
-	$('#home').click(function(){
-		window.location = '/';
-	});
 
-	 });
+function getJson(url, callback){
+	$.ajax({
+		url: url,
+		type:'GET',
+		success: callback
+	});
 }
