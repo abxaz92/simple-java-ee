4991877f46b13dc3e67c2325096bf4b5175979cb
david <chaava01@gmail.com>
2016-04-17 14:35:54 +0300

    some css

---
+++ src/main/java/local/diplom/service/common/CORSFilter.java
@@ -0,0 +1,20 @@
+package local.diplom.service.common;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+/**
+ * Created  by david on 17.04.16
+ */
+public class CORSFilter implements ContainerResponseFilter {
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext cres) throws IOException {
+        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+        cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
