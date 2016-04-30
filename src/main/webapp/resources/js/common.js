87807aec14c5f9530450e981745f10552458c3a5
david <chaava01@gmail.com>
2016-04-30 15:06:56 +0300

    drop down list

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -35,25 +35,43 @@ function getJson(url, callback) {
         success: callback
     });
 }
-
-function post(url, data, callback) {
-    $.ajax({
-        url: host + url,
-        type: 'POST',
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: callback
-    });
-}
-
-function put(url, data, callback) {
-    $.ajax({
-        url: host + url,
-        type: 'PUT',
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: callback
-    });
+Controller = {
+    get: function (url, data, callback) {
+        $.ajax({
+            url: host + url,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            success: callback
+        });
+    },
+    post: function (url, data, callback) {
+        $.ajax({
+            url: host + url,
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: callback
+        });
+    },
+    put: function (url, data, callback) {
+        $.ajax({
+            url: host + url,
+            type: 'PUT',
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: callback
+        });
+    },
+    del: function (url, data, callback) {
+        $.ajax({
+            url: host + url,
+            type: 'DELETE',
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: callback
+        });
+    }
 }
