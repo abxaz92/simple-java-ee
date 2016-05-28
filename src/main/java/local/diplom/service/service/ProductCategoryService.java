bee29079ab988641698e8e3be923c26a7f128937
david <chaava01@gmail.com>
2016-05-28 17:06:04 +0300

    comments

--- src/main/java/local/diplom/service/service/ProductCategoryService.java
+++ src/main/java/local/diplom/service/service/ProductCategoryService.java
@@ -8,15 +8,23 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
- * Created by david on 27.02.16.
+ * Сервис для Категорий продуктов
  */
 @ApplicationScoped
 public class ProductCategoryService extends AbstractDAO<ProductCategory> {
 
+    /**
+     * Инициализация от каркаса работы с БД
+     * при инициализации указываем название таблицы и
+     * сущность которую БД должна возвращать
+     */
     public ProductCategoryService() {
         super("ProductCategory", ProductCategory.class);
     }
 
+    /**
+     * Получение списка продуктов из БД
+     */
     public List<ProductCategory> findAll() {
         TypedQuery<ProductCategory> namedQuery = em.createNamedQuery("ProductCategory.getAll", ProductCategory.class);
         return namedQuery.getResultList();
--- src/main/java/local/diplom/service/service/ProductService.java
+++ src/main/java/local/diplom/service/service/ProductService.java
@@ -21,98 +21,107 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by david on 22.02.16 .
+ * Сервис для Продуктов
  */
 
 @ApplicationScoped
 public class ProductService extends AbstractDAO<Product> {
     private static final Logger log = LoggerFactory.getLogger(SaleProductController.class);
 
+    /**
+     * Инициализация от каркаса работы с БД
+     * при инициализации указываем название таблицы и
+     * сущность которую БД должна возвращать
+     */
     public ProductService() {
         super("Product", Product.class);
     }
 
+    // метод загрузки изображение в БД
     public void uploadImage(@PathParam("id") Long id, MultipartFormDataInput input) throws Exception {
-        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
-        uploadForm.forEach((key, val) -> {
-            InputPart inputPart = val.get(0);
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap(); // Получение списка файлов
+        uploadForm.forEach((key, val) -> { // Получение списка файлов
+            InputPart inputPart = val.get(0); // Получение списка файлов
             try (InputStream inputStream = inputPart.getBody(InputStream.class, null)) {
-                Product product = findById(id);
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                Product product = findById(id); // достаем товар по id
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream(); // чтение файла в память
                 int nRead;
                 byte[] data = new byte[16384];
                 while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
                     buffer.write(data, 0, nRead);
                 }
                 buffer.flush();
-                Image image = null;
-                if (product.getImage() != null && !product.getImage().isEmpty()) {
-                    image = em.find(Image.class, Long.parseLong(product.getImage()));
+                Image image = null; // инициализации сущности изображения
+                if (product.getImage() != null && !product.getImage().isEmpty()) { // если у товара уже есть изображение
+                    image = em.find(Image.class, Long.parseLong(product.getImage())); // то достаем его из БД
                 }
                 try {
-                    utx.begin();
-                    if (image == null) {
-                        image = new Image();
-                        image.setImageFile(buffer.toByteArray());
-                        em.persist(image);
-                    } else {
-                        image.setImageFile(buffer.toByteArray());
-                        em.merge(image);
+                    utx.begin(); // начало транзакции
+                    if (image == null) {  // если у товара нет изображения
+                        image = new Image(); // создаем новое
+                        image.setImageFile(buffer.toByteArray()); // добавляем файл в сущность изображение
+                        em.persist(image); // сохраняем изображение в БД
+                    } else { // иначе если у товара уже есть изображение
+                        image.setImageFile(buffer.toByteArray()); // добавляем файл в сущность изображение
+                        em.merge(image); // сохраняем изображение в БД
                     }
-                    product.setImage(String.valueOf(image.getId()));
-                    em.merge(product);
-                    utx.commit();
+                    product.setImage(String.valueOf(image.getId())); // указываем товару ID только что созданного изображения
+                    em.merge(product); // сохраняем товар в БД
+                    utx.commit(); // завершаем транзакцию
                 } catch (Exception e) {
-                    utx.rollback();
+                    utx.rollback(); // в случае ошибки откатываем транзакцию
                     e.printStackTrace();
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                e.printStackTrace(); // в случае ошибки возвращаем клиенту ошибку
                 throw exception(Response.Status.SERVICE_UNAVAILABLE, "Что-то пошло не так");
             }
 
         });
     }
 
+    // Метод выборки товаров из БД
     public Object findAll(Integer skip, Integer limit, String count, Long category) {
+        // Если не запрашивается количество товаров
         if (count == null) {
             Query query = null;
-            if (category == null)
-                query = em.createNamedQuery(tablename + ".getAll", Product.class);
-            else {
+            if (category == null) // если не указан фильтр по категории
+                query = em.createNamedQuery(tablename + ".getAll", Product.class); // достаем все товары из БД
+            else { // иначе если фильтр по категориям указан, то создаем запрос с категорией
                 query = em.createQuery("SELECT p FROM " + tablename + " p WHERE amount > 0 AND category_id = " + category);
             }
-            if (skip != null)
+            if (skip != null) // если указано с какой позиции вернуть документы
                 query.setFirstResult(skip);
-            if (limit != null)
+            if (limit != null) // если указано сколько документов вернуть
                 query.setMaxResults(limit);
-            return query.getResultList();
+            return query.getResultList(); // возвращаем список
         } else {
-            String sql = "SELECT COUNT(d) FROM " + tablename + " d";
-            Query query = em.createQuery(sql);
-            return query.getSingleResult();
+            String sql = "SELECT COUNT(d) FROM " + tablename + " d"; // если запросили количесво документов в БД
+            Query query = em.createQuery(sql); // создаем запрос
+            return query.getSingleResult(); // возвращаем ответ
         }
     }
 
     @Override
+    // Удаление товара из БД
     public void deleteById(Long id) throws Exception {
         try {
-            utx.begin();
-            Product product = findById(id);
-            em.remove(product);
-            if (product.getImage() != null) {
+            utx.begin(); // начало транзакции
+            Product product = findById(id); // достаем товар из БД
+            em.remove(product); // удаляем товар
+            if (product.getImage() != null) { // если в товаре указано изображение, то удаляем его
                 try {
-                    Image image = em.find(Image.class, Long.parseLong(product.getImage()));
-                    if (image != null)
-                        em.remove(image);
+                    Image image = em.find(Image.class, Long.parseLong(product.getImage())); // дсотаем изображение из БД
+                    if (image != null) // если изображение есть в БД
+                        em.remove(image); // удаляем изображение
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            utx.commit();
+            utx.commit(); // завершаем транзакцию
         } catch (Exception e) {
             e.printStackTrace();
-            utx.rollback();
+            utx.rollback(); // в случае ошибки отказываем транзакицю
         }
     }
 
