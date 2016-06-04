900d43411d6c394313c90128d95a4dda558f2c08
david <chaava01@gmail.com>
2016-06-04 15:41:34 +0300

    tabs

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -98,3 +98,30 @@ Controller = {
         });
     }
 }
+
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    try {
+        evt.currentTarget.className += " active";
+    } catch (e) {
+        console.debug(e);
+    }
+}
+
+function getFormattedDate(date) {
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    return year + '-' + month + '-' + day;
+}
